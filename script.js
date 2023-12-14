4b5969e5392bfce109c67f994018ef50f18f6e95
DEBARSI MONDAL <141905446+mDebarsi@users.noreply.github.com>
2023-12-14 21:47:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
+	// Check if sampleObject is defined
+  if (typeof sampleObject !== 'undefined' && sampleObject !== null) {
+    // Check if the key exists in the sampleObject
+    return key in sampleObject;
+  } else {
+    // If sampleObject is not defined, return false
+    return false;
+  }
 }
 
 // Do not change the code below
